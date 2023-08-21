e5e99bebb2bf8b7dca67605aefdf42632a5ace27
unknown <tgdd.hieu2005@gmail.com>
2023-08-21 19:30:29 +0700

    baitap

--- bai8/baitap/baitap2/baitap2.js
+++ bai8/baitap/baitap2/baitap2.js
@@ -1,3 +1,3 @@
-let nhietdoc= +prompt("nhietdoc",nhietdoC)
-constnhietdof
-document.write("diem do f",)
+let c = Number(prompt("moi nhap do c"))
+let f = (9*c/5) + 32
+console.log("do f",f);
--- bai8/baitap/baitap3/baitap3.js
+++ bai8/baitap/baitap3/baitap3.js
@@ -1,1 +1,6 @@
-
+const PI = 3.14
+let radius = Number(prompt("nhap ban kinh"))
+let result = radius* radius*PI; 
+let resul = 2 * radius * PI;
+console.log(`dien tich hinh tron bankinh ${radius} :`,result);
+console.log(`chu vi hinh tron bankinh ${radius} :`,resul);
